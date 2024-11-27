788f963d0c49f0af027715a8baf847ac9f9ade38
Mennah Shaker <mennahmohamedshaker0@gmail.com>
2024-11-27 10:43:26 +0200

    Register + Register File

--- src/main/java/com/tomasolu/classes/Register.java
+++ src/main/java/com/tomasolu/classes/Register.java
@@ -5,4 +5,31 @@ public class Register {
     String Q;
     // has value and Q field , value either double or integer depending
     // name is index
+    
+    public Register(double value, String Q) {
+        this.value = value;
+        this.Q = Q;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getQ() {
+        return Q;
+    }
+
+    public void setQ(String q) {
+        Q = q;
+    }
+
+    public boolean isReady() {
+        return Q.isEmpty();
+    }
+
+
 }
--- src/main/java/com/tomasolu/classes/RegisterFile.java
+++ src/main/java/com/tomasolu/classes/RegisterFile.java
@@ -4,4 +4,63 @@ public class RegisterFile {
     // 2 lists of registers (object) of size 32 each
     Register[] R;
     Register[] F;
+
+    public RegisterFile() {
+        R = new Register[32];
+        F = new Register[32];
+        for (int i = 0; i < 32; i++) {
+            R[i] = new Register(0, ""); //integer value reg
+            F[i] = new Register(0.0, ""); //float value reg
+        }
+    }
+
+    public Register getIntRegister (int index) { //name of reg is its index in arrau
+        return R[index];
+    }
+
+    public Register getFloatRegister (int index) {
+        return F[index];
+    }
+
+    public void setFloatRegister(int index, Register value){
+        F[index] = value;
+        F[index].setQ("");
+    }
+
+    public void setIntegerRegister(int index, Register value){
+        R[index] = value;
+        R[index].setQ("");
+    }
+
+    public Register[] getR() {
+        return R;
+    }
+
+    public void setR(Register[] r) {
+        R = r;
+    }
+
+    public Register[] getF() {
+        return F;
+    }
+
+    public void setF(Register[] f) {
+        F = f;
+    }
+
+    public void updateIntegerRegister(int index, double value, String Q) {
+        if (R[index].isReady()) {
+            R[index].setValue(value);
+        } else {
+            R[index].setQ(Q);
+        }
+    }
+
+    public void updateFloatingRegister(int index, double value, String Q) {
+        if (F[index].isReady()) {
+            F[index].setValue(value);
+        } else {
+            F[index].setQ(Q);
+        }
+    }
 }
